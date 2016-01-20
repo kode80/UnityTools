ab80b6524353c928f9c565de28d2fce6e87096ec
Ben Hopkins <ben@kode80.com>
2016-01-19 22:40:48 -0500

    Implemented recording in RecordVideo.

--- Assets/kode80/EditorTools/Scripts/RecordVideo.cs
+++ Assets/kode80/EditorTools/Scripts/RecordVideo.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.IO;
 using System.Collections;
 
 namespace kode80.EditorTools
@@ -9,15 +10,48 @@ namespace kode80.EditorTools
 	[AddComponentMenu("")]
 	public class RecordVideo : MonoBehaviour 
 	{
+		public int captureFramerate = 60;
+		public int superSize = 1;
+		public string folderPath;
 
-		// Use this for initialization
-		void Start () {
-		
+		private bool _isRecording = false;
+		public bool isRecording { get { return _isRecording; } }
+		private int _sceneNumber = 0;
+		private int _frameNumber = 0;
+
+		void Start () 
+		{
+			Time.captureFramerate = captureFramerate;
 		}
-		
-		// Update is called once per frame
-		void Update () {
-		
+
+		void Update () 
+		{
+			if( _isRecording)
+			{
+				string path = string.Format("{0}/Scene{1:D03}Frame{2:D08}.png", folderPath, _sceneNumber, _frameNumber );
+				Application.CaptureScreenshot( path, superSize);
+				_frameNumber++;
+			}
+		}
+
+		public bool StartRecording()
+		{
+			if( _isRecording == false && Directory.Exists( folderPath))
+			{
+				_isRecording = true;
+			}
+
+			return _isRecording;
+		}
+
+		public void StopRecording()
+		{
+			if( _isRecording == true)
+			{
+				_isRecording = false;
+				_sceneNumber++;
+				_frameNumber = 0;
+			}
 		}
 	}
 }
