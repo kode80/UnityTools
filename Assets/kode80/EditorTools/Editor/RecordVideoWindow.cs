0e141e963ce43621039838cfc4a42704aad7665f
Ben Hopkins <ben@kode80.com>
2016-01-20 14:35:25 -0500

    * Added GUIEnumPopup to GUIWrapper * Implemented frames/MP4/GIF export in RecordVideoWindow

--- Assets/kode80/EditorTools/Editor/RecordVideoWindow.cs
+++ Assets/kode80/EditorTools/Editor/RecordVideoWindow.cs
@@ -14,6 +14,14 @@ namespace kode80.EditorTools
 		const string OutputFolderPrefsKey = "kode80.EditorTools.RecordVideoWindow.OutputFolder";
 		const string SuperSizePrefsKey = "kode80.EditorTools.RecordVideoWindow.SuperSize";
 		const string FrameratePrefsKey = "kode80.EditorTools.RecordVideoWindow.Framerate";
+		const string OutputFormatPrefsKey = "kode80.EditorTools.RecordVideoWindow.OutputFormat";
+
+		enum OutputFormat
+		{
+			Frames = 0,
+			FramesAndMP4 = 1,
+			FramesAndGIF = 2
+		}
 
 		private GUIVertical _gui;
 		private GUIButton _recordButton;
@@ -42,6 +50,9 @@ namespace kode80.EditorTools
 			_gui.Add( new GUIIntSlider( new GUIContent( "Framerate", 
 														"The fixed framerate of recorded video"), 
 										_recordVideo.captureFramerate, 10, 60, FramerateChanged));
+			_gui.Add( new GUIEnumPopup( new GUIContent( "Output Format"), 
+										(OutputFormat) EditorPrefs.GetInt( OutputFormatPrefsKey), 
+										OutputFormatChanged));
 			_gui.Add( new GUISpace());
 			_recordButton = _gui.Add( new GUIButton( new GUIContent( "Record"), RecordClicked)) as GUIButton;
 			_recordButton.isEnabled = EditorApplication.isPlaying;
@@ -116,6 +127,12 @@ namespace kode80.EditorTools
 			_recordVideo.captureFramerate = slider.value;
 		}
 
+		void OutputFormatChanged( GUIBase sender)
+		{
+			GUIEnumPopup popup = sender as GUIEnumPopup;
+			EditorPrefs.SetInt( OutputFormatPrefsKey, (int)(OutputFormat)popup.value);
+		}
+
 		void RecordClicked( GUIBase sender)
 		{
 			ToggleRecording();
@@ -132,7 +149,11 @@ namespace kode80.EditorTools
 
 			if( EditorApplication.isPlayingOrWillChangePlaymode == false && EditorApplication.isPlaying == false)
 			{
-				CompileVideo( 0);
+				OutputFormat format = (OutputFormat) EditorPrefs.GetInt( OutputFormatPrefsKey);
+				if( format != OutputFormat.Frames)
+				{
+					CompileVideo( 0, format == OutputFormat.FramesAndGIF);
+				}
 			}
 		}
 
@@ -145,6 +166,7 @@ namespace kode80.EditorTools
 				EditorPrefs.DeleteKey( OutputFolderPrefsKey);
 				EditorPrefs.SetInt( SuperSizePrefsKey, 1);
 				EditorPrefs.SetInt( FrameratePrefsKey, 60);
+				EditorPrefs.SetInt( OutputFormatPrefsKey, (int)OutputFormat.FramesAndMP4);
 				EditorPrefs.SetBool( HasInitializedPrefsKey, true);
 			}
 		}
@@ -199,7 +221,7 @@ namespace kode80.EditorTools
 			Repaint();
 		}
 
-		void CompileVideo( int sceneNumber)
+		void CompileVideo( int sceneNumber, bool createGif=false)
 		{
 			string path = EditorPrefs.GetString( FFmpegPathPrefsKey);
 
@@ -207,7 +229,10 @@ namespace kode80.EditorTools
 			{
 				int framerate = EditorPrefs.GetInt( FrameratePrefsKey);
 				string outputPath = EditorPrefs.GetString( OutputFolderPrefsKey);
-				string args = string.Format( "-i \"Scene{0:D03}Frame%08d.png\" -y -c:v libx264 -pix_fmt yuv420p -r {1} -preset ultrafast \"Scene{2:D03}.mp4\"", 
+				string args = createGif ?
+							  string.Format( "-i \"Scene{0:D03}Frame%08d.png\" -y \"Scene{1:D03}.gif\"", 
+											 sceneNumber, sceneNumber) :
+							  string.Format( "-i \"Scene{0:D03}Frame%08d.png\" -y -c:v libx264 -pix_fmt yuv420p -r {1} -preset ultrafast \"Scene{2:D03}.mp4\"", 
 						 					 sceneNumber, framerate, sceneNumber);
 				
 				Process process = new Process();
