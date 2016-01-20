38dcc9410574d1780951f3a64bea538df99a2f6a
Ben Hopkins <ben@kode80.com>
2016-01-19 22:18:11 -0500

    Added stubbed RecordVideoWindow and RecordVideo classes.

---
+++ Assets/kode80/EditorTools/Editor/RecordVideoWindow.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace kode80.EditorTools
+{
+	public class RecordVideoWindow : EditorWindow
+	{
+		[MenuItem( "Window/kode80/Editor Tools/Record Video")]
+		public static void Init()
+		{
+			RecordVideoWindow win = EditorWindow.GetWindow( typeof( RecordVideoWindow)) as RecordVideoWindow;
+			win.titleContent = new GUIContent( "Record Video");
+			win.Show();
+		}
+	}
+}
---
+++ Assets/kode80/EditorTools/Scripts/RecordVideo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace kode80.EditorTools
+{
+	// The RecordVideo component is used by RecordVideoWindow
+	// to record frames during play mode and should't be added
+	// manually by the user, so hide it from the component menu.
+	[AddComponentMenu("")]
+	public class RecordVideo : MonoBehaviour 
+	{
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+	}
+}
