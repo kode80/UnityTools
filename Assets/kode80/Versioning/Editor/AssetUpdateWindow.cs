17736721f671b0c615fc56d60cc1730ef71f0551
Ben Hopkins <ben@kode80.com>
2016-04-12 13:54:51 -0400

    Added initial versioning classes

---
+++ Assets/kode80/Versioning/Editor/AssetUpdateWindow.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Xml;
+using System.Net;
+using kode80.GUIWrapper;
+
+namespace kode80.Versioning
+{
+	public class AssetUpdateWindow : EditorWindow 
+	{
+		private GUIVertical _gui;
+
+		[MenuItem( "Window/kode80/Update")]
+		public static void Init()
+		{
+			AssetUpdateWindow win = EditorWindow.GetWindow( typeof( AssetUpdateWindow)) as AssetUpdateWindow;
+			win.titleContent = new GUIContent( "Asset Update");
+			win.Show();
+		}
+
+		void OnEnable()
+		{
+			Uri versionURI = new Uri( "http://kode80.com/assets/version/UnityTools.xml");
+			Debug.Log( versionURI);
+
+			WebClient client = new WebClient();
+			string xmlString = client.DownloadString( versionURI);
+
+			AssetVersion assetVersion = AssetVersion.ParseXML( xmlString);
+			if( assetVersion != null)
+			{
+				Debug.Log( assetVersion);
+			}
+		}
+	}
+}
---
+++ Assets/kode80/Versioning/Editor/AssetVersion.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Xml;
+
+namespace kode80.Versioning
+{
+	public class AssetVersion
+	{
+		public string Name { get; private set; }
+		public string Author { get; private set; }
+		public SemanticVersion Version { get; private set; }
+		public string Notes { get; private set; }
+		public Uri Uri { get; private set; }
+
+		public static AssetVersion ParseXML( string xmlString)
+		{
+			XmlDocument xml = new XmlDocument();
+
+			try { xml.LoadXml( xmlString); }
+			catch( XmlException e) { return null; }
+
+			XmlNode name = xml.SelectSingleNode( "asset/name");
+			XmlNode author = xml.SelectSingleNode( "asset/author");
+			XmlNode version = xml.SelectSingleNode( "asset/version");
+			XmlNode notes = xml.SelectSingleNode( "asset/notes");
+			XmlNode uri = xml.SelectSingleNode( "asset/uri");
+
+			if( name == null || author == null || version == null || notes == null || uri == null) {
+				Debug.Log( "Error parsing Asset Version XML");
+				return null;
+			}
+
+			SemanticVersion semanticVersion = SemanticVersion.Parse( version.InnerText);
+			if( semanticVersion == null) {
+				Debug.Log( "Error parsing Semantic Version");
+				return null;
+			}
+
+			AssetVersion assetVersion = new AssetVersion();
+			assetVersion.Name = name.InnerText;
+			assetVersion.Author = author.InnerText;
+			assetVersion.Version = semanticVersion;
+			assetVersion.Notes = notes.InnerText;
+			assetVersion.Uri = new Uri( uri.InnerText);
+
+			return assetVersion;
+		}
+
+		public override string ToString()
+		{
+			return "Name: " + Name + "\n" +
+				"Author: " + Author + "\n" +
+				"Version: " + Version + "\n" +
+				"Notes: " + Notes + "\n" +
+				"URI: " + Uri;
+		}
+	}
+}
