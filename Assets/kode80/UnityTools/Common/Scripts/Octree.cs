def154d363a0bbb27b98c133a50b2f53e6ae0fd3
Ben Hopkins <ben@kode80.com>
2016-07-04 15:26:09 -0400

    Stubbed out generic Octree

--- Assets/kode80/UnityTools/Common/Scripts/Octree.cs
+++ Assets/kode80/UnityTools/Common/Scripts/Octree.cs
@@ -3,7 +3,33 @@ using System.Collections;
 
 namespace kode80.Common
 {
-	public class Octree
+	public class Octree<T>
 	{
+		private OctreeNode<T> rootNode;
+
+		private int maxDepth;
+		public int MaxDepth {
+			get { return maxDepth; }
+			set {
+				if( maxDepth != value)
+				{
+					maxDepth = value;
+					Regenerate();
+				}
+			}
+		}
+
+		public Bounds Bounds { get { return rootNode.Bounds; } }
+
+		public Octree( Bounds bounds, OctreeNode<T>.ItemOverlapsBounds itemOverlapsBounds, int maxDepth = 1)
+		{
+			rootNode = new OctreeNode<T>( bounds, itemOverlapsBounds);
+			MaxDepth = maxDepth;
+		}
+
+		private void Regenerate()
+		{
+			rootNode.Collapse();
+		}
 	}
 }
