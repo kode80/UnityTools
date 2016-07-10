21f333a16edfe187ce68727e20895499320cebff
Ben Hopkins <ben@kode80.com>
2016-07-10 11:13:29 -0400

    Added early Diff/DiffWindow classes

---
+++ Assets/kode80/UnityTools/EditorTools/Editor/Diff.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace kode80.EditorTools
+{
+	public struct DiffRecord
+	{
+		public GameObject gameObjectA;
+		public GameObject gameObjectB;
+		public string propertyName;
+		public string gameObjectPath;
+		public SerializedPropertyType propertyType;
+	}
+
+	public class Diff
+	{
+		private Vector2 scrollPosition;
+
+		public void Compare( GameObject gameObjectA, GameObject gameObjectB, List<DiffRecord> diffProperties, string gameObjectPath = "")
+		{
+			if( gameObjectPath == "") {
+				gameObjectPath = gameObjectA.name;
+			}
+			else {
+				gameObjectPath += "." + gameObjectA.name;
+			}
+
+			var componentsA = gameObjectA.GetComponents<Component>();
+			var componentsB = gameObjectB.GetComponents<Component>();
+
+			if( componentsA.Length != componentsB.Length)
+			{
+				return;
+			}
+
+			int count = componentsA.Length;
+
+			for( int i=0; i<count; i++)
+			{
+				var serializedA = new SerializedObject( componentsA[i]);
+				var serializedB = new SerializedObject( componentsB[i]);
+				var iterA = serializedA.GetIterator();
+				var iterB = serializedB.GetIterator();
+				SerializedProperty p;
+				while( iterA.NextVisible( true) && iterB.NextVisible( true))
+				{
+					if( iterA.name == iterB.name &&
+						iterA.propertyType == iterB.propertyType)
+					{
+						bool isDiff = CompareValues( iterA, iterB) == false;
+
+						if( isDiff && IsPropertyParentType( iterA) == false)
+						{
+							var record = new DiffRecord();
+							record.gameObjectA = gameObjectA;
+							record.gameObjectB = gameObjectB;
+							record.propertyName = iterA.propertyPath;
+							record.propertyType = iterA.propertyType;
+							record.gameObjectPath = gameObjectPath;
+							diffProperties.Add( record);
+						}
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+
+			var childCountA = gameObjectA.transform.childCount;
+			var childCountB = gameObjectA.transform.childCount;
+
+			if( childCountA != childCountB) {
+				return;
+			}
+
+			for( int j=0; j<childCountA; j++) 
+			{
+				Compare( gameObjectA.transform.GetChild( j).gameObject,
+					gameObjectB.transform.GetChild( j).gameObject,
+					diffProperties, 
+					gameObjectPath);
+			}
+		}
+
+		private bool CompareValues( SerializedProperty propertyA, SerializedProperty propertyB)
+		{
+			if( propertyA.propertyType != propertyB.propertyType) {
+				return false;
+			}
+
+			switch( propertyA.propertyType)
+			{
+			case SerializedPropertyType.Integer:
+				return propertyA.intValue == propertyB.intValue;
+
+			case SerializedPropertyType.Boolean:
+				return propertyA.boolValue == propertyB.boolValue;
+
+			case SerializedPropertyType.Float:
+				return propertyA.floatValue == propertyB.floatValue;
+				
+			case SerializedPropertyType.String:
+				return propertyA.stringValue == propertyB.stringValue;
+				
+			case SerializedPropertyType.Color:
+				return propertyA.colorValue == propertyB.colorValue;
+				
+			case SerializedPropertyType.ObjectReference:
+				return propertyA.objectReferenceValue == propertyB.objectReferenceValue;
+				
+			case SerializedPropertyType.LayerMask:
+				return propertyA.intValue == propertyB.intValue;
+				
+			case SerializedPropertyType.Enum:
+				return propertyA.enumValueIndex == propertyB.enumValueIndex;
+				
+			case SerializedPropertyType.Vector2:
+				return propertyA.vector2Value == propertyB.vector2Value;
+				
+			case SerializedPropertyType.Vector3:
+				return propertyA.vector3Value == propertyB.vector3Value;
+				
+			case SerializedPropertyType.Vector4:
+				return propertyA.vector4Value == propertyB.vector4Value;
+				
+			case SerializedPropertyType.Rect:
+				return propertyA.rectValue == propertyB.rectValue;
+				
+			case SerializedPropertyType.ArraySize:
+				return propertyA.arraySize == propertyB.arraySize;
+				
+			case SerializedPropertyType.Character:
+				return propertyA.intValue == propertyB.intValue;
+				
+			case SerializedPropertyType.AnimationCurve:
+				return propertyA.animationCurveValue == propertyB.animationCurveValue;
+				
+			case SerializedPropertyType.Bounds:
+				return propertyA.boundsValue == propertyB.boundsValue;
+				
+			case SerializedPropertyType.Gradient:
+				return propertyA.objectReferenceValue == propertyB.objectReferenceValue;
+				
+			case SerializedPropertyType.Quaternion:
+				return propertyA.quaternionValue == propertyB.quaternionValue;
+			}
+
+			return false;
+		}
+
+		private bool IsPropertyParentType( SerializedProperty property)
+		{
+			switch( property.propertyType)
+			{
+			case SerializedPropertyType.Vector2:
+			case SerializedPropertyType.Vector3:
+			case SerializedPropertyType.Vector4:
+			case SerializedPropertyType.Rect:
+			case SerializedPropertyType.Bounds:
+			case SerializedPropertyType.Quaternion:
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
---
+++ Assets/kode80/UnityTools/EditorTools/Editor/DiffWindow.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace kode80.EditorTools
+{
+	public class DiffWindow : EditorWindow 
+	{
+		private GameObject gameObjectA;
+		private GameObject gameObjectB;
+		private Vector2 scrollPosition;
+		private List<DiffRecord> diffs;
+		private int selectedIndex = -1;
+		private int stopHighlightCount;
+
+		[MenuItem( "Window/kode80/Editor Tools/Diff")]
+		public static void Init()
+		{
+			EditorWindow.GetWindow<DiffWindow>( "Diff").Show();
+		}
+
+		void OnEnable()
+		{
+		}
+
+		void OnDisable()
+		{
+		}
+
+		void OnGUI()
+		{
+			if( diffs == null) {
+				diffs = new List<DiffRecord>();
+			}
+
+			bool refresh = false;
+			var newGameObject = EditorGUILayout.ObjectField( gameObjectA, typeof(GameObject), true) as GameObject;
+			if( newGameObject != gameObjectA)
+			{
+				gameObjectA = newGameObject;
+				refresh = true;
+			}
+
+			newGameObject = EditorGUILayout.ObjectField( gameObjectB, typeof(GameObject), true) as GameObject;
+			if( newGameObject != gameObjectB)
+			{
+				gameObjectB = newGameObject;
+				refresh = true;
+			}
+
+			if( GUILayout.Button( "Refresh")) {
+				refresh = true;
+			}
+
+			if( refresh && 
+				gameObjectA != null &&
+				gameObjectB != null)
+			{
+				diffs.Clear();
+				selectedIndex = -1;
+
+				var diff = new Diff();
+				diff.Compare( gameObjectA, gameObjectB, diffs);
+			}
+
+
+			scrollPosition = EditorGUILayout.BeginScrollView( scrollPosition);
+
+			int count = diffs.Count;
+
+			for( int i=0; i<count; i++)
+			{
+				var record = diffs[ i];
+
+				EditorGUILayout.BeginVertical();
+
+				EditorGUILayout.LabelField( record.gameObjectPath + "." + record.propertyName + " (" + record.propertyType + ") ");
+
+				EditorGUILayout.BeginHorizontal();
+				EditorGUILayout.BeginVertical();
+				if( GUILayout.Button( "Select " + record.gameObjectA.name)) {
+					Selection.activeGameObject = record.gameObjectA;
+					selectedIndex = i;
+				}
+				EditorGUILayout.EndVertical();
+
+				EditorGUILayout.BeginVertical();
+				if( GUILayout.Button( "Select " + record.gameObjectB.name)) {
+					Selection.activeGameObject = record.gameObjectB;
+					selectedIndex = i;
+				}
+				EditorGUILayout.EndVertical();
+				EditorGUILayout.EndHorizontal();
+
+				EditorGUILayout.EndVertical();
+				EditorGUILayout.Space();
+			}
+
+			EditorGUILayout.EndScrollView();
+		}
+
+		void OnSelectionChange()
+		{
+			if( selectedIndex > -1)
+			{
+				Highlighter.Highlight( "Inspector", diffs[selectedIndex].propertyName);
+				stopHighlightCount = 20;
+			}
+		}
+
+		void Update()
+		{
+			if( stopHighlightCount > 0)
+			{
+				stopHighlightCount--;
+				if( stopHighlightCount == 0)
+				{
+					Highlighter.Stop();
+				}
+			}
+		}
+	}
+}
